123b88b20fee9d06c3eb2917f4ce7aab8ca944e5
MayconEidam <127507968+MayconEidam@users.noreply.github.com>
2024-10-16 11:50:12 -0300

    ok

--- Graficos/informacoesGlobais.js
+++ Graficos/informacoesGlobais.js
@@ -1,17 +1,13 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-async function visualizarInformacoesGlobais() {
-    const res = await fetch(url)
-    const dados = await res.json()
-    const paragrafo = document.createElemente('p')
-    paragrafo.classList.add('graficos-container_texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem
-     ${dados.total_pessoas_mundo} de pessoas e
-     que aproximadamente ${dados.total_pessoas_conectadas}
-     estão conectadas em alguma rede social e passam em média
-     ${dados.tempo_medio} horas conectadas.`
+async function visualizarInformacoesGlobais (){
+const res = await fetch (url)
+const dados = await res.json()
+const paragrafo = document.createElement ('p')
+paragrafo.classList.add('graficos-container_texto')
+paragrafo.innerHTML = `Voce sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectados} estão conectados em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
 
-     const container = document.getElementByld('graficos-container')
-     container.appendChild(paragrafo)
+const container = document.getElementById( 'graficos-container')
+container.appendChild(paragrafo)
 }
 visualizarInformacoesGlobais()
