49509308abc03cf1ade652f1cf6bd87e8891ac55
MayconEidam <127507968+MayconEidam@users.noreply.github.com>
2024-10-22 08:00:15 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,7 +6,7 @@ async function visualizarInformacoesGlobais() {
         const paragrafo = document.createElement('p')
         paragrafo.classList.add('graficos-container__texto')
         paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span>
-        de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> que estão 
+        de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas} bilhões</span> que estão 
         conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> que horas conectadas.`
 
         const container = document.getElementById ('graficos-container')
